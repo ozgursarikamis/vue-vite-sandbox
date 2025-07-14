a4f4313707c5332c263c052bed25265ebb940a4e
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-07-15 00:55:03 +0300

    Tailwind: Configuration

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+  // tailwind.config.js
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'class' or false
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
