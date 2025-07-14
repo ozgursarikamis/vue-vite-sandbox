0531eda48932abf7ce94b22f97d6953b894b3a4f
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-07-14 23:25:21 +0300

    v-if, v-else

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
-<script setup>
-
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: 'test'
+    }
+  }
+}
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <h1>Hello {{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'inactive'">User is inactive</p>
+  <p v-else>User status is unknown</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    
+  </ul>
 </template>
 
 <style scoped>
