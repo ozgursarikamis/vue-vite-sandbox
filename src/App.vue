d3d483cabea5ea1e03dcf48ca0be52f230696ff6
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-07-17 00:21:50 +0300

    `vue-router` Configuration

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
-import Hero from './components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
-import JobListings from './components/JobListings.vue';
+import { RouterView } from 'vue-router';
 
 </script>
 <template>
   <Navbar />
-  <Hero />
-  <HomeCards />
-  <JobListings :showButton="true" />
+  <RouterView />
 </template>
 
 <style scoped></style>
