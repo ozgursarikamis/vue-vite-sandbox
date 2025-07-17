d68c41c92344577ae1f1db797854ca2694a9d3ba
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-07-17 13:03:17 +0300

    `reactive` instead of `ref`

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
-import jobData from '../jobs.json';
-import { ref } from 'vue';
+import { defineProps, onMounted, reactive } from 'vue';
+
 import JobListing from './JobListing.vue';
 import { RouterLink } from 'vue-router';
 
-const jobs = ref(jobData);
-console.log([...jobs.value]);
-console.log(JSON.parse(JSON.stringify(jobs.value)));
+import axios from 'axios';
+
+// const jobs = ref([]);
+// console.log([...jobs.value]);
+// console.log(JSON.parse(JSON.stringify(jobs.value)));
 
 defineProps({
     limit: {
@@ -20,6 +21,22 @@ defineProps({
     }
 });
 
+const state = reactive({
+    jobs: [],
+    isLoading: true
+});
+
+onMounted(async () => {
+    try {
+        const response = await axios.get('http://localhost:8000/jobs');
+        state.jobs = response.data;
+    } catch (error) {
+        console.error('error', error);
+    } finally {
+        state.isLoading = false;
+    }
+});
+
 </script>
 
 <template>
@@ -29,11 +46,13 @@ defineProps({
                 Browse Jobs
             </h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit)" :key="job.id" :job="job" />
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" 
+                    :key="job.id"
+                    :job="job" />
             </div>
         </div>
     </section>
-    
+
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
         <RouterLink to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
             View All Jobs
