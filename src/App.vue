5db186725210ab8ca5dc31c974b96a7c6dca9afe
Ozgur Sarikamis <ozgur.sarikamis@gmail.com>
2025-07-28 01:56:10 +0300

    Logout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
+import { ref } from 'vue';
+import { useAuth } from './composables/useAuth';
+
+const auth = ref(useAuth());
 </script>
 
 <template>
+  <div>
+    <div v-if="auth.isAuthenticated">
+      <button @click="auth.logout()">Logout</button>
+    </div>
+    <div v-else>
+      <button @click="auth.login()">Login</button>
+    </div>
+  </div>
   <RouterLink to="/">Home</RouterLink> |
   <RouterLink to="/about">About</RouterLink> | 
   <RouterLink to="/courses">Courses</RouterLink>
--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,14 +1,18 @@
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 const isAuthenticated = ref(false);
 
 export function useAuth() {
+
+    const router = useRouter();
     function login() {
         isAuthenticated.value = true;
     }
 
     function logout() {
         isAuthenticated.value = false;
+        router.push('/');
     }
 
     return {
